c6994c7ce351bcce4610c479ac0a5959dafff71d
Dieter Verhulst <dieter.verhulst@student.hogent.be>
2021-11-24 21:46:17 +0100

    Add project files.

---
+++ BlazorConwayGameOfLife/Models/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorConwayGameOfLife.Models
+{
+    public enum Status
+    {
+        Dead,
+        Alive,
+    }
+}
